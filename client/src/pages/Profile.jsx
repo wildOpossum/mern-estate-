5581b73c6711085b25568a51b77417d374aeeb7b
Vetal <sonovetal@gmail.com>
2025-12-25 14:18:37 +0200

    completed profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import { useRef } from 'react'
+import { useSelector } from 'react-redux';
 
 const Profile = () => {
+  const fileRef = useRef(null);
+  const currentUser = useSelector((state) => state.user);
+
+  const handleSubmit = () => {
+
+  };
+
+  const setFile = (e) => {
+
+  };
+
+  const handleChange = () => {
+
+  };
+
+  const handleDeleteUser = () => {
+
+  };
+
+  const handleSignOut = () => {
+
+  };
+
   return (
-	<div>Profile</div>
+	<div className='p-3 max-w-lg mx-auto'>
+    <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type='file'
+          ref={fileRef}
+          hidden
+          accept='image/*'
+        />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={currentUser.avatar}
+          alt='profile'
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+        <input
+          type='text'
+          placeholder='username'
+          defaultValue={currentUser.username}
+          id='username'
+          className='border p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <input
+          type='email'
+          placeholder='email'
+          id='email'
+          defaultValue={currentUser.email}
+          className='border p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          onChange={handleChange}
+          id='password'
+          className='border p-3 rounded-lg'
+        />
+        <button
+          className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
+        >Update</button>
+    </form>
+    <div className='flex justify-between mt-5'>
+        <span
+          onClick={handleDeleteUser}
+          className='text-red-700 cursor-pointer'
+        >
+          Delete account
+        </span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
+          Sign out
+        </span>
+      </div>
+  </div>
   )
 }
 
